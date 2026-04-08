087d22c5de7bddfd8fde2d576199ea4c56d67397
Karthik Yadav <karthikgudaru72@gmail.com>
2026-04-08 22:25:47 +0530

    go day-2

--- main.go
+++ main.go
@@ -5,18 +5,53 @@ import (
 )
 
 func main() {
-	var name string
-	var age int
-	fmt.Print("Enter you'r name : ")
-	fmt.Scanln(&name)
-	fmt.Print("Enter you'r age : ")
+    greet()
+}
 
-	_,err := fmt.Scanln(&age)
 
-	if err != nil {
-		fmt.Println("error",err)
-		return
+func reverse() {
+	var slice = make([]int,6,10)
+    fmt.Print(slice)
+	fmt.Println()
+	for i := 0; i < len(slice)/2; i++ {
+        temp := slice[i]
+		slice[i] = slice[len(slice)-1-i]
+		slice[len(slice)-1-i] = temp
 	}
+	fmt.Print(slice, len(slice), cap(slice))
+}
+
+func even_odd() {
+	var num int
+    fmt.Print("Enter the number : ")
+	fmt.Scanln(&num)
+	if num % 2 == 0 {
+		fmt.Printf("%d is even",num)
+	}else {
+		fmt.Printf("%d is odd",num)
+	}
+}
+
+func greet() {
+	var name string
+    fmt.Print("Enter your name : ")
+	fmt.Scanln(&name)
+	var age int
+    fmt.Print("Enter your age : ")
+	fmt.Scanln(&age)
 	
 	fmt.Printf("Hey %s! kudos to turning %d in hits year",name,age+1)
+}
+
+func sum() {
+	var num int
+    fmt.Print("Enter the number : ")
+	fmt.Scanln(&num)
+
+	var sum int
+	for i := 1; i <= num; i++ {
+		sum += i
+	}
+
+	fmt.Printf("Sum of 1st %d is %d",num,sum)
 }
